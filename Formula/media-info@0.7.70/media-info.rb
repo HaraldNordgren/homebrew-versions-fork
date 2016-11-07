234c86eff40fac5986b1d28871742143dc4ddacd
HaraldNordgren <haraldnordgren@gmail.com>
2016-11-07 03:39:11 +0100

    Adding formula for Mediainfo 0.7.70

---
+++ Formula/media-info@0.7.70/media-info.rb
@@ -0,0 +1,44 @@
+class MediaInfo < Formula
+  desc "Unified display of technical and tag data for audio/video"
+  homepage "https://mediaarea.net/"
+  url "https://mediaarea.net/download/binary/mediainfo/0.7.70/MediaInfo_CLI_0.7.70_GNU_FromSource.tar.bz2"
+  version "0.7.70"
+  sha256 "af60c1e965c5a23ab96de4b7a7a3f9a7eeafe4d5b0e48f75d1ad1ea86ff0dae7"
+
+  depends_on "pkg-config" => :build
+  # fails to build against Leopard's older libcurl
+  depends_on "curl" if MacOS.version < :snow_leopard
+
+  def install
+    cd "ZenLib/Project/GNU/Library" do
+      args = ["--disable-debug",
+              "--disable-dependency-tracking",
+              "--enable-static",
+              "--enable-shared",
+              "--prefix=#{prefix}"]
+      system "./configure", *args
+      system "make", "install"
+    end
+
+    cd "MediaInfoLib/Project/GNU/Library" do
+      args = ["--disable-debug",
+              "--disable-dependency-tracking",
+              "--with-libcurl",
+              "--enable-static",
+              "--enable-shared",
+              "--prefix=#{prefix}"]
+      system "./configure", *args
+      system "make", "install"
+    end
+
+    cd "MediaInfo/Project/GNU/CLI" do
+      system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                            "--prefix=#{prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    pipe_output("#{bin}/mediainfo", test_fixtures("test.mp3"))
+  end
+end
