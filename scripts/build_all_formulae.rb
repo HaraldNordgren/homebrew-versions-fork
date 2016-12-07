18d88fdf53e61ba7db1b8ea705a2f61584dceae1
HaraldNordgren <haraldnordgren@gmail.com>
2016-12-07 22:19:11 +0100

    Test script that works for both -versions and -versions-reference

---
+++ scripts/build_all_formulae.rb
@@ -0,0 +1,113 @@
+require 'fileutils'
+
+
+debug_skip = false
+
+
+REPO_SLUG = ENV['TRAVIS_REPO_SLUG']
+puts "REPO_SLUG: #{REPO_SLUG}"
+
+author, tap_name = REPO_SLUG.split("/")
+tap_short_name = tap_name.split("-")[1..-1].join("-")
+
+tap_cmd = "brew tap #{author}/#{tap_short_name}"
+successful_exit = system(tap_cmd)
+if not successful_exit
+    puts "Cannot tap with cmd: '#{tap_cmd}'"
+    exit 1
+end
+
+tap_dir = "/usr/local/Homebrew/Library/Taps/#{author}/homebrew-#{tap_short_name}"
+puts tap_dir
+
+skip_packages_string = ARGV[0]
+
+if tap_short_name == 'versions-fork'
+    skip_packages_string = ARGV[0].gsub("-@", "-").gsub("@", "")
+    formula_glob = "#{tap_dir}/*.rb"
+    shorten_formula = lambda { |file_name|
+        return File.basename(file_name, File.extname(file_name))
+    }
+elsif tap_short_name == 'versions'
+    skip_packages_string = ARGV[0].gsub("-@", "@")
+    formula_glob = "#{tap_dir}/Aliases/*"
+    shorten_formula = lambda { |file_name|
+        return File.basename(file_name)
+    }
+end
+
+skip_packages = skip_packages_string.split(" ")
+
+timing_out = [
+    /allegro[@]?[0-9]+/,
+    /arangodb[@]?[0-9]+/,
+    /android-ndk/,
+    /bazel[@]?[0-9]+/,
+    /boost[@]?[0-9]+/,
+    /bootst-python[@]?[0-9]+/,
+    /duplicity[@]?06/,
+    /erlang(@|\-)?r[0-9]+/,
+    /ffmpeg[@]?[0-9]+/,
+]
+
+cmd = ""
+
+puts "Finding formulas through glob: '#{formula_glob}"
+for file_name in Dir[formula_glob]
+    #puts "Matched #{file_name}"
+
+    file_without_extension = shorten_formula.call(file_name)
+    #puts "Tranformed to: #{file_without_extension}"
+
+    #if file_without_extension == skip_packages.last
+    if file_without_extension == 'ffmpeg28'
+        debug_skip = false
+    end
+
+    if debug_skip
+        puts "SKIPPING AHEAD OVER #{file_without_extension}"
+        next
+    end
+
+    if skip_packages.include?(file_without_extension)
+        puts "SKIPPING #{file_without_extension}"
+        next
+    end
+
+    matched_regex = false
+    for regex in timing_out
+        if file_without_extension =~ regex
+            puts "SKIPPING #{file_without_extension} after matching #{regex}"
+            matched_regex = true
+            break
+        end
+    end
+
+    if matched_regex
+        next
+    end
+
+    package_full_name = "#{author}/#{tap_short_name}/#{file_without_extension}"
+    
+    cmd += "echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% && "
+    cmd += "echo && "
+    cmd += "echo Installing #{file_without_extension} && "
+
+    if file_without_extension =~ /automake/
+        cmd += "brew link autoconf && "
+    end
+
+    cmd += "brew install #{package_full_name} && "
+    cmd += "brew unlink #{package_full_name} && "
+    cmd += "brew uninstall #{package_full_name} && "
+    
+    if file_without_extension =~ /automake/
+        cmd += "brew unlink autoconf && "
+    end
+end
+
+cmd += "echo ALL BUILDS FINISHED"
+successful_exit = system(cmd)
+
+exit successful_exit ? 0 : 1
+
