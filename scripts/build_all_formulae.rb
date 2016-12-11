9fd68fce30755397be418eb3b0cde2959d611529
HaraldNordgren <haraldnordgren@gmail.com>
2016-12-11 14:49:45 +0100

    Unlinking conflicting versions before install

--- scripts/build_all_formulae.rb
+++ scripts/build_all_formulae.rb
@@ -121,16 +121,27 @@ for file_name in Dir[formula_glob]
 
     if file_without_extension =~ /automake/
         cmd_list.push("brew link autoconf")
-    elsif file_without_extension =~ /gdal[@]?111|influxdb[@]?08|ledger[@]?26|llvm[@]?35/
+    elsif file_without_extension =~ /influxdb[@]?08|ledger[@]?26|llvm[@]?35|thrift[@]?090|riak[@]?132|rebar[@]?3|rabbitmq[@]?30/
         cmd_list.push("brew link autoconf automake")
+    elsif file_without_extension =~ /gdal[@]?111/
+        cmd_list.push("brew link autoconf automake")
+        cmd_list.push("brew unlink gdal")
     elsif file_without_extension =~ /gnupg[@]?21/
         cmd_list.push("brew unlink gnupg2 gpg-agent dirmngr")
     elsif file_without_extension =~ /go[@]?15/
         cmd_list.push("brew unlink go")
-    elsif file_without_extension =~ /jpeg[@]?gb/
+    elsif file_without_extension =~ /jpeg[@]?gb|jpeg[@]?6b/
         cmd_list.push("brew unlink jpeg")
     elsif file_without_extension =~ /ruby[@]?192/
         cmd_list.push("brew unlink ruby")
+    elsif file_without_extension =~ /subversion[@]?18/
+        cmd_list.push("brew unlink subversion")
+    elsif file_without_extension =~ /povray[@]?36/
+        cmd_list.push("brew unlink libpng")
+    elsif file_without_extension =~ /postgresql[@]?[0-9]+/
+        cmd_list.push("brew unlink postgresql")
+    elsif file_without_extension =~ /lz4[@]?r117/
+        cmd_list.push("brew unlink lz4")
     end
 
     cmd_list.push("brew install --verbose #{package_full_name}")
@@ -139,7 +150,9 @@ for file_name in Dir[formula_glob]
 
     if file_without_extension =~ /automake/
         cmd_list.push("brew unlink autoconf")
-    elsif file_without_extension =~ /gdal[@]?111|influxdb[@]?08|ledger[@]?26|llvm[@]?35/
+    elsif file_without_extension =~ /influxdb[@]?08|ledger[@]?26|llvm[@]?35|thrift[@]?090|riak[@]?132|rebar[@]?3|rabbitmq[@]?30/
+        cmd_list.push("brew unlink autoconf automake")
+    elsif file_without_extension =~ /gdal[@]?111/
         cmd_list.push("brew unlink autoconf automake")
     end
 
